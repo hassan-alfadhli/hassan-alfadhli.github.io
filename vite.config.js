10ea22c37615355cce8f5478cc52367235c114bc
Modar M. Alfadly <modar.alfadly@gmail.com>
2023-10-10 17:04:31 +0300

    Restructure with vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+    root: path.resolve(__dirname, "src"),
+    build: {
+        outDir: "../docs",
+        emptyOutDir: true,
+    },
+    publicDir: "../public",
+    server: {
+        port: 8080,
+    },
+});
