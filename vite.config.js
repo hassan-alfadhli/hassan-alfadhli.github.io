087fd1cadb23ac37dcb47592ab46f60618b73164
xmodar <modar.alfadly@gmail.com>
2023-10-13 02:51:49 +0300

    Add .editorconfig

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,32 @@ import { defineConfig } from "vite";
 import path from "path";
 import purgecss from "@fullhuman/postcss-purgecss";
 
-export default defineConfig({
-    root: path.resolve(__dirname, "src"),
-    build: {
-        outDir: "../docs",
-        emptyOutDir: true,
-    },
-    publicDir: "../public",
-    server: {
-        port: 8080,
-    },
-    css: {
-        postcss: {
-            plugins: [
-                purgecss({
-                    content: ["./**/*.html"],
-                    safelist: ["data-bs-theme"],
-                }),
-            ],
+export default defineConfig(({ command }) => {
+    const config = {
+        root: path.resolve(__dirname, "src"),
+        build: {
+            outDir: "../docs",
+            emptyOutDir: true,
         },
-    },
+        publicDir: "../public",
+        server: {
+            port: 8080,
+        },
+        css: {
+            postcss: {
+                plugins: [
+
+                ],
+            },
+        },
+    };
+    if (command === "build") {
+        config.css.postcss.plugins.push(
+            purgecss({
+                content: ["./**/*.html"],
+                safelist: ["data-bs-theme"],
+            }),
+        );
+    }
+    return config;
 });
